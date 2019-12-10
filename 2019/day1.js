eabc7876a4eb86f3cea3eb7ca02390246be71f5d
Radu Albastroiu <radu.albastroiu@caphyon.com>
2019-12-10 19:31:43 +0200

    Day 1

--- 2019/day1.js
+++ 2019/day1.js
@@ -2,7 +2,16 @@
 
 function main(input)
 {
-  console.log(input);
+  let result = 0;
+  let values = input.split("\n")
+  values.forEach(value => {
+    while(value > 0)
+    {
+      value = Math.max(Math.floor(parseInt(value, 10) / 3) - 2, 0);
+      result += value;
+    }
+  });
+  console.log(result);
 }
 
 module.exports = {main};
